05d8f2e316f17903a4fed20b171786f7dc86a29b
gulman <gulman@users.noreply.github.com>
2015-04-15 02:53:18 -0400

    Update tempreversi.c

--- tempreversi.c
+++ tempreversi.c
@@ -26,8 +26,7 @@
  * - To avoid confusion, we will use (i, j) to refer to input/output indexes and (row, col) to refer to the indexes
  *   of our "internal" board implementation. Note that only the COLUMN index has to be modified.
  *
- * - The mathematical relationship between the external and internal indexing is [col = j + abs(4 - i)]
- *   and [j = col - abs(4 - row)] (the inverse function).
+ * - Indexes start from 1 (row/i range is 1-8 and col/j range is 1-14)
  *
  * - I have implemented two helper functions to convert both ways between (i, j) and (row, col).
  *
@@ -36,7 +35,7 @@
  *
  * QUESTIONS
  * ---------
- * - Do cell indexes (i, j) start from 0 or 1? For now, I am going to assume 1.
+ * -
  *
  *
  *
@@ -66,11 +65,17 @@ int main(void) {
  *	Declare array to hold our move and initialize with zeros
  *	move[0] holds row index ('i' OR 'row') and move[1] holds column index ('j' OR 'col')
  *
- *	Note: The initial value of 0 tells us that the array has not been modified yet.
- *	However, if we find that the (i, j) indexes start from 0, then we will change this
- *	to initialize with -1.
+ *	Note: Since 0 is not a valid index, we can use these initial 0 values to indicate/check that
+ *	      the array has not been modified yet.
  */
 	signed char move[2] = {0, 0};
+
+/*
+ * Declare 2d array to hold input board and initialize with zeros
+ *
+ * Note: This array might be replaced later with a struct
+*/
+	signed char b[8][14] = {{0}};
 
 
 /*
@@ -80,6 +85,9 @@ int main(void) {
 
 
 	// TODO Using loops, scan the board for empty cells that represent a legal move and store them.
+
+
+	// At this point we are done with the requirements for the five submit server tests.
 
 
 	// TODO This is where the logic starts for computing the best move
@@ -107,11 +115,31 @@ int main(void) {
 // Convert column index from external to internal implementation
 signed char toCol(signed char i, signed char j) {
 
-	return (j + abs(4 - i));
+	if (i >= 1 && i < 4)
+		return (j + (4 - i));
+	else if (i == 4 || i == 5)
+		return (j);
+	else if (i >= 6 && i <=8)
+		return (j + (i - 4));
+	else {
+
+		printf("Index/es sent to function toCol are out of range!");
+		return 0;
+	}
 }
 
 // Convert column index from internal to external implementation
 signed char toJ(signed char row, signed char col) {
 
-	return (col - abs(4 - row));
+	if (row >= 1 && row < 4)
+		return (col + (2*row - 8));
+	else if (row == 4 || row == 5)
+		return (col);
+	else if (row > 5 && row <=8)
+		return (col - (2*row - 10));
+	else {
+
+		printf("Index/es sent to function toJ are out of range!");
+		return 0;
+	}
 }
